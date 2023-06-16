a12f76398eda5df17936aa14a5316839436c8efe
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-06-16 12:00:02 -0500

    clase empleado push 5

--- src/empleado.java
+++ src/empleado.java
@@ -1,66 +1,9 @@
 public class empleado {
-    String nombreEmpleado;
-    String apellidoEmpleado;
-    String cedulaEmpleado;
-    String cargoEmpleado;
-    float sueldoEmpleado;
-    String codigoEmpleado;
-
-    public empleado(String nombreEmpleado, String apellidoEmpleado, String cedulaEmpleado, String cargoEmpleado, float sueldoEmpleado, String codigoEmpleado) {
-        this.nombreEmpleado = nombreEmpleado;
-        this.apellidoEmpleado = apellidoEmpleado;
-        this.cedulaEmpleado = cedulaEmpleado;
-        this.cargoEmpleado = cargoEmpleado;
-        this.sueldoEmpleado = sueldoEmpleado;
-        this.codigoEmpleado = codigoEmpleado;
-    }
-
-    public String getNombreEmpleado() {
-        return nombreEmpleado;
-    }
-
-    public void setNombreEmpleado(String nombreEmpleado) {
-        this.nombreEmpleado = nombreEmpleado;
-    }
-
-    public String getApellidoEmpleado() {
-        return apellidoEmpleado;
-    }
-
-    public void setApellidoEmpleado(String apellidoEmpleado) {
-        this.apellidoEmpleado = apellidoEmpleado;
-    }
-
-    public String getCedulaEmpleado() {
-        return cedulaEmpleado;
-    }
-
-    public void setCedulaEmpleado(String cedulaEmpleado) {
-        this.cedulaEmpleado = cedulaEmpleado;
-    }
-
-    public String getCargoEmpleado() {
-        return cargoEmpleado;
-    }
-
-    public void setCargoEmpleado(String cargoEmpleado) {
-        this.cargoEmpleado = cargoEmpleado;
-    }
-
-    public float getSueldoEmpleado() {
-        return sueldoEmpleado;
-    }
-
-    public void setSueldoEmpleado(float sueldoEmpleado) {
-        this.sueldoEmpleado = sueldoEmpleado;
-    }
-
-    public String getCodigoEmpleado() {
-        return codigoEmpleado;
-    }
-
-    public void setCodigoEmpleado(String codigoEmpleado) {
-        this.codigoEmpleado = codigoEmpleado;
-    }
+    String nombre_Empleado;
+    String apellido_Empleado;
+    String cedula_Empleado;
+    String cargo_Empleado;
+    float sueldo_Empleado;
+    String codigo_Empleado;
 }
 
