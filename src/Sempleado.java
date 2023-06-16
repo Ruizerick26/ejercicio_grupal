9c2fbda0e38a950198ac4c640d9a165add04b29a
Ruizerick26 <ruizerick2525@gmail.com>
2023-06-16 12:27:40 -0500

    Administrativo corregido

--- src/Sempleado.java
+++ src/Sempleado.java
@@ -1,43 +1,33 @@
 public class Sempleado extends empleado {
 
     //Atributos
-    private String s_nombre;
-    private String s_apellido;
-    private int s_edad;
+    private String departamento;
+    private String Equipo_C;
 
     //Constructor
 
 
-    public Sempleado(String nombre_Empleado, String apellido_Empleado, String cedula_Empleado, String cargo_Empleado, float sueldo_Empleado, String codigo_Empleado, String s_nombre, String s_apellido, int s_edad) {
+    public Sempleado(String nombre_Empleado, String apellido_Empleado, String cedula_Empleado, String cargo_Empleado, float sueldo_Empleado, String codigo_Empleado, String departamento, String Equipo_C) {
         super(nombre_Empleado, apellido_Empleado, cedula_Empleado, cargo_Empleado, sueldo_Empleado, codigo_Empleado);
-        this.s_nombre = s_nombre;
-        this.s_apellido = s_apellido;
-        this.s_edad = s_edad;
+        this.departamento = departamento;
+        this.Equipo_C = Equipo_C;
     }
     //setter y getters
 
 
-    public String getS_nombre() {
-        return s_nombre;
+    public String getDepartamento() {
+        return departamento;
     }
 
-    public void setS_nombre(String s_nombre) {
-        this.s_nombre = s_nombre;
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
     }
 
-    public String getS_apellido() {
-        return s_apellido;
+    public String getEquipo_C() {
+        return Equipo_C;
     }
 
-    public void setS_apellido(String s_apellido) {
-        this.s_apellido = s_apellido;
-    }
-
-    public int getS_edad() {
-        return s_edad;
-    }
-
-    public void setS_edad(int s_edad) {
-        this.s_edad = s_edad;
+    public void setEquipo_C(String equipo_C) {
+        Equipo_C = equipo_C;
     }
 }
