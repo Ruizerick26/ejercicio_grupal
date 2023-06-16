7a7f34ee1f4709c1ec1b9916b47aeb1b4f52c1fa
Orland H <24heyerunsu@gmail.com>
2023-06-16 12:48:23 -0500

    Menu 3

--- src/menu.java
+++ src/menu.java
@@ -26,28 +26,26 @@ public class menu {
         System.out.println("------------------------------------------------");
         for (docentes profe : profes){
             System.out.print("Ingrese el nombre: ");
-            String aux = sc.nextLine();
+            String aux = sc.next();
             profe.setNombre_Empleado(aux);
             System.out.print("Ingrese el apellido: ");
-            aux = sc.nextLine();
+            aux = sc.next();
             profe.setApellido_Empleado(aux);
             System.out.print("Ingrese la cedula: ");
-            aux = sc.nextLine();
+            aux = sc.next();
             profe.setCedula_Empleado(aux);
             System.out.print("Ingrese el cargo del empleado: ");
-            aux = sc.nextLine();
+            aux = sc.next();
             profe.setCargo_Empleado(aux);
             System.out.print("Ingrese el sueldo: ");
             float aux2 = sc.nextFloat();
             profe.setSueldo_Empleado(aux2);
             System.out.print("Ingrese el codigo: ");
-            String aux3 = sc.nextLine();
+            String aux3 = sc.next();
             profe.setCodigo_Empleado(aux3);
-            System.out.print("Ingrese el cargo del empleado: ");
-            aux3 = sc.nextLine();
-            profe.setCargo_Empleado(aux3);
             System.out.println("Siguiente:----------------------");
         }
+
 
         //Setters para ingresar de administrativos
         System.out.println("------------------------------------------------");
@@ -55,23 +53,27 @@ public class menu {
         System.out.println("------------------------------------------------");
         for (Sempleado admini : admin){
             System.out.print("Ingrese el nombre: ");
-            String aux = sc.nextLine();
+            String aux = sc.next();
             admini.setNombre_Empleado(aux);
             System.out.print("Ingrese el apellido: ");
-            aux = sc.nextLine();
+            aux = sc.next();
             admini.setApellido_Empleado(aux);
             System.out.print("Ingrese la cedula: ");
-            aux = sc.nextLine();
+            aux = sc.next();
             admini.setCedula_Empleado(aux);
             System.out.print("Ingrese el cargo del empleado: ");
-            aux = sc.nextLine();
+            aux = sc.next();
             admini.setCargo_Empleado(aux);
             System.out.print("Ingrese el sueldo: ");
             float aux2 = sc.nextFloat();
             admini.setSueldo_Empleado(aux2);
             System.out.print("Ingrese el codigo: ");
-            aux = sc.nextLine();
+            aux = sc.next();
             admini.setCodigo_Empleado(aux);
+            System.out.print("Ingrese el departamento:");
+            aux = sc.next();
+            admini.setDepartamento(aux);
+            System.out.println("Siguiente:----------------------");
         }
     }
 }
