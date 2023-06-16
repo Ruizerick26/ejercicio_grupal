4759ac1901a0f98488f7cfb35fbd47744b074306
Mateo <minora.mateo@gmail.com>
2023-06-16 12:11:44 -0500

    no message

--- src/Sempleado.java
+++ src/Sempleado.java
@@ -1,4 +1,25 @@
-public class Sempleado extends empleado{
+public class Sempleado extends empleado {
 
+    //Atributos
+    private String s_nombre;
+    private String s_apellido;
+    private int s_edad;
 
+    //Constructor
+
+    public Sempleado(String nombre_Empleado,
+                     String apellido_Empleado,
+                     String cedula_Empleado,
+                     String cargo_Empleado,
+                     float sueldo_Empleado,
+
+                     String codigo_Empleado,
+                     String nombre,
+                     String apellido, int edad)
+    {
+        super(nombre_Empleado, apellido_Empleado, cedula_Empleado, cargo_Empleado, sueldo_Empleado, codigo_Empleado);
+        this.s_nombre = nombre;
+        this.s_apellido = apellido;
+        this.s_edad = edad;
+    }
 }
